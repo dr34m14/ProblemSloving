b41ea4e11297f76e2d065f1e6c54d4f0d250bc58
shopno <shopnoshutrodhar@gmail.com>
2022-12-28 12:02:17 +0600

    testcase 11 fix

--- leetcode/remove_stones_to_minimize_the_total.js
+++ leetcode/remove_stones_to_minimize_the_total.js
@@ -3,7 +3,7 @@ let k = 100000
 var minStoneSum = function (piles, k) {
     let min = 0;
     piles.sort(function (a, b) { return b - a });
-    if (piles.length > 1) {
+    if (piles[0]!= 1 ) {
 
         for (let i = 0; i < k; i++) {
             piles[i] = Math.ceil(piles[i] / 2);
