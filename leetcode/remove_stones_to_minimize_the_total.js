ade27dd667d2365c27c20f7ea367541c918a0f5d
shopno <shopnoshutrodhar@gmail.com>
2022-12-28 11:44:01 +0600

    erro fixted testcase 10

--- leetcode/remove_stones_to_minimize_the_total.js
+++ leetcode/remove_stones_to_minimize_the_total.js
@@ -1,16 +1,17 @@
-let piles = [5, 4, 9];
-let k = 2
+let piles = [1];
+let k = 100000
 var minStoneSum = function (piles, k) {
     let min = 0;
     piles.sort(function (a, b) { return b - a });
-    console.log(piles)
+    if (piles.length > 1) {
+
         for (let i = 0; i < k; i++) {
             piles[i] = Math.ceil(piles[i] / 2);
 
 
         }
-    
-    console.log(piles)
+    }
+
     for (let j = 0; j < piles.length; j++) {
         min = min + piles[j];
     }
