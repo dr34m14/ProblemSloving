dcbffd7335e723c130fb23f4d50a4dc91d83bcaa
shopno <shopnoshutrodhar@gmail.com>
2022-12-28 12:26:57 +0600

    testcase 11 -_-

--- leetcode/remove_stones_to_minimize_the_total.js
+++ leetcode/remove_stones_to_minimize_the_total.js
@@ -1,19 +1,27 @@
-let piles = [1];
-let k = 100000
+let piles = [4122, 9928, 3477, 9942];
+let k = 6
 var minStoneSum = function (piles, k) {
     let min = 0;
+
+    if (piles[0].constructor === Array) {
+        piles = piles[0];
+    }
     piles.sort(function (a, b) { return b - a });
-    if (piles.length>1 ) {
+    if (piles.length > 1 && piles.length > k) {
 
         for (let i = 0; i < k; i++) {
             piles[i] = Math.ceil(piles[i] / 2);
 
 
         }
-    }else{
-        while(k--){
-            piles[0] = Math.ceil(piles[0] / 2);
- 
+    } else {
+        let i = 0;
+        console.log(piles,k)
+        while (k--&&i<=k) {
+                piles[i] = Math.ceil(piles[i] / 2);
+
+
+            i++;
         }
     }
 
